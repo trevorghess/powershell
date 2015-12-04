d4dbb60f4c2c28446611da7ca1b670949b9b231b
Steven Murawski <steven.murawski@gmail.com>
2015-12-04 11:28:52 -0600

    fix the tests for the dsc and winrm recipes on Windows

--- spec/recipes/winrm_spec.rb
+++ spec/recipes/winrm_spec.rb
@@ -6,7 +6,7 @@ describe 'powershell::winrm' do
     ChefSpec::SoloRunner.new(platform: 'windows', version: '2012').converge(described_recipe)
   end
 
-  it 'installs windows package windows managemet framework core 5.0' do
+  it 'configures the winrm endpoint with quickconfig' do
     expect(chef_run).to run_powershell_script('enable winrm').with(code: "      winrm quickconfig -q\n")
   end
 end
