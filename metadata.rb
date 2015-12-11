2ae15d802c68be0b96281e42a4f3a7b70f901dce
Steven Murawski <steven.murawski@gmail.com>
2015-12-11 16:25:35 -0600

    updated metadata for major version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures PowerShell on the Windows platform'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '3.2.0'
+version '4.0.0'
 
 recipe 'powershell::default', 'Makes sure RubyZip is installed (for powershell_module)'
 recipe 'powershell::powershell2', 'Installs PowerShell 2.0'
@@ -16,10 +16,10 @@ recipe 'powershell::dsc', 'Desired State Configuration'
 recipe 'powershell::enable_lcm', 'Enable the DSC Local Configuration Manager'
 recipe 'powershell::disable_lcm', 'Disable the DSC Local Configuration Manager'
 
-supports 'windows'
-depends 'windows', '>= 1.2.8'
-depends 'ms_dotnet', '>= 2.6'
-depends 'chef_handler'
+supports  'windows'
+depends   'windows',    '>= 1.38'
+depends   'ms_dotnet',  '>= 2.6'
+depends   'chef_handler'
 
 source_url 'https://github.com/chef-cookbooks/powershell' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/powershell/issues' if respond_to?(:issues_url)
